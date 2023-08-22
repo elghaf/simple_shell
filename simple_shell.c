c8cd48a30789da48d5e9e7b16682b58307968a1a
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-08-22 14:13:08 +0100

    qcad

--- simple_shell.c
+++ simple_shell.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 #include <string.h> // Include the string.h header for strcpy function
+#include <unistd.h>
 
-int main(int ac, char **av) {
+#define MAX_COMAND_LENGTH 1024
+#define MAX_PATH_LENGTH 1024
 
-    // Initialize character arrays
-    char *result = av[0]; // This should be a pointer, not an array
-    char result1[20];     // Declare character array with size 20
-    char result2[20];     // Declare character array with size 20
+int main() 
+{
+char input_text[MAX_COMAND_LENGTH];
+char *args[MAX_COMAND_LENGTH];
 
-    // Copy the values of av[1] and av[2] into result1 and result2
-    strcpy(result1, av[1]);
-    strcpy(result2, av[2]);
+// path : 
+char *path = "/bin";
 
-    // Print the values of the character arrays
-    printf("Result: %s\n", result);
-    printf("Result1: %s\n", result1);
-    printf("Result2: %s\n", result2);
 
-    return 0;
+
 }
