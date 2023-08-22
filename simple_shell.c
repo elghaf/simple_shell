4a4b2ec2b46d9884e5d427868cea8e32b5c528e9
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-08-22 16:47:58 +0100

    Update simple_shell.c

--- simple_shell.c
+++ simple_shell.c
@@ -1,38 +1,77 @@
 #include <stdio.h>
-#include <string.h> // Include the string.h header for strcpy function
-#include <unistd.h>
-
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
+#define MAX_INPUT_LENGTH 1024
 
-
-#define MAX_COMAND_LENGTH 1024
-#define MAX_PATH_LENGTH 1024
-
-int main() 
-{
-char input_text[MAX_COMAND_LENGTH];
-char *args[MAX_COMAND_LENGTH];
-size_t n;
-
-// path : 
-char *path = "/bin";
-
-while (1)
-{
-    /* code */
-    printf("#ayoub~~:");
-    //allocate some place memory for the use to input MAX8COMMAND
-    input_text = (char *)malloc(n * sizeof(char));
-    if(input_text == NULL)
-    {
-        perror("Memory error");
-        return (EXIT_FAILURE);
+// Function to display the shell prompt
+void display_prompt(void) {
+    if (isatty(STDIN_FILENO)) {
+        printf("#cisfun$ ");
+        fflush(stdout);  // Ensure prompt is displayed immediately
     }
-    //now lets read the uer input with getline function
-    
 }
 
+// Function to execute a command in a child process
+void execute_command(char *command) {
+    pid_t pid;
+    int status;
 
+    pid = fork();
+    if (pid == 0) {
+        // Child process
+        char *args[] = {command, NULL};  // Arguments for execve
+
+        // Execute the command
+        execvp(command, args);
+
+        // Print error if execvp fails
+        perror("Execution error");
+        exit(1);
+    } else if (pid > 0) {
+        // Parent process
+        waitpid(pid, &status, 0);  // Wait for the child to finish
+    } else {
+        // Fork failed
+        perror("Fork error");
+    }
+}
+
+int main(void) {
+    char input[MAX_INPUT_LENGTH];
+    ssize_t input_length;
+
+    while (1) {
+        // Display the shell prompt
+        display_prompt();
+
+        // Read user input
+        input_length = read(STDIN_FILENO, input, MAX_INPUT_LENGTH);
+
+        // Handle EOF (Ctrl+D)
+        if (input_length <= 0) {
+            if (isatty(STDIN_FILENO)) {
+                printf("exit\n");
+            }
+            break;  // Exit the loop
+        }
+
+        // Remove trailing newline
+        if (input[input_length - 1] == '\n') {
+            input[input_length - 1] = '\0';
+        }
+
+        // Skip empty input lines
+        if (strlen(input) == 0) {
+            continue;  // Skip to the next iteration
+        }
+
+        // Execute the command
+        execute_command(input);
+    }
+
+    return 0;
 }
