9d08ac5eb647c29045aca4c99cea7fc9e6e70c2c
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-08-23 00:58:50 +0100

    add

---
+++ shell_main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Entry to shell command
+ * @ac: command arg count
+ * @arv: command arg array
+ * 
+ * Return: 0 if success -1 otherwise null
+ * 
+ */
+
+int main(int ac, char **arv)
+{
+    
+}
