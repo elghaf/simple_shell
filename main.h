38eb8d0cd8687b71183d769a09ad47e5cc6dbb1d
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-08-23 01:26:34 +0100

    Update main.h

--- main.h
+++ main.h
@@ -5,10 +5,10 @@
 #include <stdlib.h>
 
 void display_prompt(const char *prompt);
-char **tokenize_input_line(char *input_line, const char *delimiter);
-int is_exit_command(const char *cmd);
-int compare_strings(const char *str1, const char *str2);
-void print_environment();
+//char **tokenize_input_line(char *input_line, const char *delimiter);
+//int is_exit_command(const char *cmd);
+//int compare_strings(const char *str1, const char *str2);
+//void print_environment();
 void execute_user_command(char **command_tokens);
 char *get_full_path(char *command);
 
