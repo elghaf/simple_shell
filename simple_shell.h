aae056e1ca519670fb0ee99aa786e7ea118f2d44
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-08-23 21:11:54 +0100

    add

--- simple_shell.h
+++ simple_shell.h
@@ -1,14 +1,14 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
 
 /* Define constants */
 #define TOKEN_BUFSIZE 64
 #define TOKEN_DELIMITERS " \t\r\n\a"
 
-
+/* Function declarations */
 char *obtainUserInput();
 char **tokenizeUserInput(char *userInput);
-int runCommand(char **arguments)
+int runCommand(char **arguments);
 void display_prompt(void);
 
 #endif
