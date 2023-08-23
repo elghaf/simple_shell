32b3dcf85af3d583b006512c3d02e4fd11093eb2
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-08-23 23:08:22 +0100

    Update main.c

--- main.c
+++ main.c
@@ -13,18 +13,17 @@
  */
 int main(void)
 {
-    char *line;
-    char **args;
+char *line;
+char **args;
 
-    while (1)
-    {
-        print_prompt();
-        line = read_input();
-        args = parse_input(line);
-        execute(args);
-        free(line);
-        free(args);
-    }
-
-    return (0);
+while (1)
+{
+print_prompt();
+line = read_input();
+args = parse_input(line);
+execute(args);
+free(line);
+free(args);
+}
+return (0);
 }
