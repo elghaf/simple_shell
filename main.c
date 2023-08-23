58de67d8c0a8e59828a4594dbc6b29dbc623af2c
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-08-23 22:59:48 +0100

    add

--- main.c
+++ main.c
@@ -2,29 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
 /**
  * main - Entry point of the simple shell program
+ * @argc: The argument count
+ * @argv: The argument vector
  *
+ * Description:
  * This function serves as the entry point for the simple shell.
- * It repeatedly prints the shell prompt, reads input, parses the input,
- * and executes the parsed arguments as a command.
  *
- * Return: Always returns 0.
+ * Return:
+ * Always returns 0.
  */
-int main(void)
+int main(int argc, char **argv)
 {
-char *line;
-char **args;
+    char *line;
+    char **args;
 
-while (1)
-{
-print_prompt();
-line = read_input();
-args = parse_input(line);
-execute(args);
-free(line);
-free(args);
-}
+    while (1)
+    {
+        print_prompt();
+        line = read_input();
+        args = parse_input(line);
+        execute(args);
+        free(line);
+        free(args);
+    }
 
-return (0);
+    return (0);
 }
