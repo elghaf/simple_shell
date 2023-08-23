9e20f835269deb5ac2808137c6164b3fd2814055
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-08-23 16:31:05 +0100

    add

--- environ.c
+++ environ.c
@@ -1,56 +1,92 @@
 #include "shell.h"
 
-char _getenv(const char *name)
+/**
+ * _myenv - prints the current environment
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int _myenv(info_t *info)
 {
-	int i, len;
-
-	len = _strlen(name);
-	for(i = 0; environ[i]; i++)
-	{
-		if (_strncmp(name, environ[i], len) == 0)
-			return(environ + i);
-	}
-	return( NULL);
+	print_list_str(info->env);
+	return (0);
 }
 
-
-char **_copyenv(void)
+/**
+ * _getenv - gets the value of an environ variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * @name: env var name
+ *
+ * Return: the value
+ */
+char *_getenv(info_t *info, const char *name)
 {
-	char **new_environ;
-	size_t size;
+	list_t *node = info->env;
+	char *p;
+
+	while (node)
+	{
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
+		node = node->next;
+	}
+	return (NULL);
+}
+
+/**
+ * _mysetenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
+ */
+int _mysetenv(info_t *info)
+{
+	if (info->argc != 3)
+	{
+		_eputs("Incorrect number of arguements\n");
+		return (1);
+	}
+	if (_setenv(info, info->argv[1], info->argv[2]))
+		return (0);
+	return (1);
+}
+
+/**
+ * _myunsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * Return: Always 0
+ */
+int _myunsetenv(info_t *info)
+{
 	int i;
 
-	for (size = 0; environ[size]; size++)
-		;
-	
-	new_environ = malloc(sizeof(char *) * (size + 1));
+	if (info->argc == 1)
+	{
+		_eputs("Too few arguements.\n");
+		return (1);
+	}
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
 
-	if (!new_environ)
-		return (NULL);
+	return (0);
+}
+
+/**
+ * populate_env_list - populates env linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int populate_env_list(info_t *info)
+{
+	list_t *node = NULL;
+	size_t i;
 
 	for (i = 0; environ[i]; i++)
-	{
-		new_environ[i] = malloc(_strlen(environ[index]) + 1);
-
-		if (!new_environ[i])
-		{
-			for (i--; i >= 0; i--)
-				free(new_environ[i]);
-			free(new_environ);
-			return (NULL);
-		}
-		_strcpy(new_environ[i], environ[i]);
-	}
-	new_environ[i] = NULL;
-
-	return (new_environ);
-}
-
-void free_env(void)
-{
-	int i;
-
-	for (i = 0; environ[i]; i++ )
-		free(environ[index]);
-	free(environ);
+		add_node_end(&node, environ[i], 0);
+	info->env = node;
+	return (0);
 }
