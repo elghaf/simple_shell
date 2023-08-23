71365f5f78fc59c61b2a18d2a5e5649f12e55bf3
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-08-23 21:14:02 +0100

    add

--- input_read.c
+++ input_read.c
@@ -1,4 +1,4 @@
-#include <simple_shell.h>
+#include "simple_shell.h"
 #include <stdlib.h>
 #include <stdio.h>
 
--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include <simple_shell.h>
+#include "simple_shell.h"
 #include <stdio.h>
 #include <stdlib.h>
 
