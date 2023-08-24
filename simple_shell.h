5f62b330959c11fe4a206a16bbe9936acd4b2d58
EL GHAFRAOUI AYOUB <52068209+elghaf@users.noreply.github.com>
2023-08-24 03:12:33 +0100

    add

---
+++ simple_shell.h
@@ -0,0 +1,10 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+
+/*function to interact with the shell*/
+void display_function(void);
+char *read_user_input();
+char **print_user_resutl(char *text_line);
+int run_command(char **args);
+
+#endif
